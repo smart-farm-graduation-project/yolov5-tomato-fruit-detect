6cc7299791736d539b13e43b0fe996e8ae804eab
oksdfn <oksdfn@gmail.com>
2023-06-02 21:49:03 +0900

    개수 추출 완료

--- detect.py
+++ detect.py
@@ -4,9 +4,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 
-model = torch.hub.load('', 'custom', path='best.pt', source='local')
-
-img = "tomato885.png"
+model = torch.hub.load('yolov5', 'custom',
+                       path='best.pt', source='local')
+# 터미널 위치 따라서 경로가 달라지니까 조심
+img = "tomato_green.jpg"
 
 results = model(img)
-results.pandas().xyxy[0]
+# results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+
+print(len(results.xyxy[0]))
